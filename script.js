1877b0a2abd2c047e6ae11543d99511d8d772450
abythomas300 <abythomas300@gmail.com>
2025-06-26 14:19:40 +0530

    feat: fetch and display employee data in table from API

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// for making table toggle on button click
 let formContainer = document.querySelector("#form-container");
 let formToggleButton = document.querySelector("#formToggleBtn");
 let isFormVisible = false;
@@ -15,3 +16,32 @@ function toggleForm(){
         isFormVisible = false;
     }
 }
+
+
+
+let tableBody = document.querySelector("#employeeTableBody")
+let rows = "";
+
+// fetch data from API
+fetch("http://localhost:3000/employees")       // fetching API
+    .then((response)=>{                        // handling http response
+        return response.json()                 // converting unreadable response body to .json format
+    })
+    .then((data)=>{                            // receiving the actual data from the promise return by json() method
+        for(let i = 0; i < data.length; i++) {
+            rows = rows + `
+        <tr>
+            <td>${i+1}</td>
+            <td>${data[i].firstName}</td>
+            <td>${data[i].email}</td>
+            <td>${data[i].phone}</td>
+            <td>${data[i].gender}</td>
+            <td>${data[i].dob}</td>
+            <td>${data[i].country}</td>
+            
+        <tr>
+        `
+        }
+        tableBody.innerHTML = rows;              // assigning all the rows value to table body 
+    })
+

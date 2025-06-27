2201bf7204c8b226e8f483ce43e857f5f8fb2bb5
abythomas300 <abythomas300@gmail.com>
2025-06-27 20:38:09 +0530

    feat: client side validation with dismissible alert box

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ function toggleForm(){
     }
 }
 
-
+// for selecting alert box
+let alertbox = document.querySelector("#alertbox");
 
 // for disabling page reload during  button click
 let employeeForm = document.querySelector("#employeeForm"); // getting form
@@ -62,6 +63,88 @@ function fetchInputDetails() {
     console.log(city);
     console.log(state);
 
+    // for converting above data into an object to send for client-side validation
+    let inputDetails = {
+        salutation,
+        firstName,
+        lastName, 
+        username, 
+        password, 
+        email, 
+        phoneno, 
+        gender, 
+        qualification, 
+        country, 
+        dob, 
+        address, 
+        city, 
+        state
+    }
+
+    console.log("Object :");
+    console.log(inputDetails)
+
+    // for sending above object for client side validation
+    validation(inputDetails);  // invoke client side validation method
+}
+
+
+// - - - - - CLIENT SIDE VALIDATION - - - - -
+
+function validation(employee) {      // the parameter 'employee' reveived 'inputDetails' send by fetchInputDetails()
+
+    const errorsLog = [];            // array for storing all error messages when one or more validation fails.
+    
+    if(!validateEmail(employee.email)) {
+        errorsLog.push("Invalid e-mail format");
+    }
+
+    if(!validatePhoneno(employee.phoneno)) {
+        errorsLog.push("Invalid phone number format");
+    }
+
+    if(!validateDate(employee.dob)) {
+        errorsLog.push("Invalid date format");
+    }
+
+    if(!validatePasswordLength(employee.password)) {
+        errorsLog.push("Password is weak");
+    }
+
+    // for logging out all found errors 
+    if(errorsLog.length > 0) {
+        alertbox.classList.remove("d-none");
+        console.log("These following errors were found :")
+        errorsLog.forEach((err)=>{
+            console.error(err);
+        })
+    } else {
+        console.log("Client Side Validation complete ✅ \n No errors found.");
+    }
+
+}
+
+// for checking whether entered email has correct format
+function validateEmail(email) {  // receives 'employee.email' parameter send from validation() method
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+// for checking whether date is in correct format 
+function validatePhoneno(phoneNo) {
+    const phonenoRegex = /^\d{10}$/;
+    return phonenoRegex.test(phoneNo)
+} 
+
+// for checking whether date is in correct format
+function validateDate(date) {
+    const dateRegex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+    return dateRegex.test(date);
+}
+
+//for checking whether password has 8 or more characters
+function validatePasswordLength(pass) { // the parameter 'pass' receives 'employee.password' parameter send by validation() method
+    return pass.length < 8? false: true;
 }
 
 
